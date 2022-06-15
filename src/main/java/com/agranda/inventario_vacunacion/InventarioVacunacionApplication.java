7dc6598d0ae31ba350a119c1b518fc34a0aa810e
Abel Granda <abelgranda@hotmail.es>
2022-06-14 21:55:03 -0500

    Primer commit con update del pom

---
+++ src/main/java/com/agranda/inventario_vacunacion/InventarioVacunacionApplication.java
@@ -0,0 +1,13 @@
+package com.agranda.inventario_vacunacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioVacunacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventarioVacunacionApplication.class, args);
+    }
+
+}
