3977421a2f724474f78dd8a814c7ac4f4a1ef1e5
Abel Granda <abelgranda@hotmail.es>
2022-06-17 18:43:24 -0500

    Correccion del  mapeo realacinal

--- src/main/java/com/agranda/inventariovacunacion/repositorio/EmpleadoRepository.java
+++ src/main/java/com/agranda/inventariovacunacion/repositorio/EmpleadoRepository.java
@@ -12,22 +12,22 @@ public interface EmpleadoRepository extends CrudRepository<Empleado, Long> {
     @Query("SELECT e FROM Empleado e WHERE e.cedula = ?1")
     Optional<Empleado> filtrarPorCedula(String cedula);
 
-    @Query("SELECT e FROM Empleado e WHERE e.estadoVacunacion = com.alefiengo.inventariovacunacionempleados.domain.enumerator.EstadoVacunacion.VACUNADO")
+    @Query("SELECT e FROM Empleado e WHERE e.estadoVacunacion = com.agranda.inventariovacunacion.dominio.enumerator.EstadoVacunacion.VACUNADO")
     Iterable<Empleado> obtenerEmpleadosVacunados();
 
-    @Query("SELECT e FROM Empleado e WHERE e.estadoVacunacion = com.alefiengo.inventariovacunacionempleados.domain.enumerator.EstadoVacunacion.NO_VACUNADO")
+    @Query("SELECT e FROM Empleado e WHERE e.estadoVacunacion = com.agranda.inventariovacunacion.dominio.enumerator.EstadoVacunacion.NO_VACUNADO")
     Iterable<Empleado> obtenerEmpleadosNoVacunados();
 
-    @Query("SELECT e FROM Empleado e JOIN e.vacuna v WHERE v.tipoVacuna = com.alefiengo.inventariovacunacionempleados.domain.enumerator.TipoVacuna.SPUTNIK_V")
+    @Query("SELECT e FROM Empleado e JOIN e.vacuna v WHERE v.tipoVacuna = com.agranda.inventariovacunacion.dominio.enumerator.TipoVacuna.SPUTNIK_V")
     Iterable<Empleado> obtenerEmpleadosSputnikV();
 
-    @Query("SELECT e FROM Empleado e JOIN e.vacuna v WHERE v.tipoVacuna = com.alefiengo.inventariovacunacionempleados.domain.enumerator.TipoVacuna.ASTRA_ZENECA")
+    @Query("SELECT e FROM Empleado e JOIN e.vacuna v WHERE v.tipoVacuna = com.agranda.inventariovacunacion.dominio.enumerator.TipoVacuna.ASTRA_ZENECA")
     Iterable<Empleado> obtenerEmpleadosAstraZeneca();
 
-    @Query("SELECT e FROM Empleado e JOIN e.vacuna v WHERE v.tipoVacuna = com.alefiengo.inventariovacunacionempleados.domain.enumerator.TipoVacuna.PFIZER")
+    @Query("SELECT e FROM Empleado e JOIN e.vacuna v WHERE v.tipoVacuna = com.agranda.inventariovacunacion.dominio.enumerator.TipoVacuna.PFIZER")
     Iterable<Empleado> obtenerEmpleadosPfizer();
 
-    @Query("SELECT e FROM Empleado e JOIN e.vacuna v WHERE v.tipoVacuna = com.alefiengo.inventariovacunacionempleados.domain.enumerator.TipoVacuna.JHONSON_AND_JHONSON")
+    @Query("SELECT e FROM Empleado e JOIN e.vacuna v WHERE v.tipoVacuna = com.agranda.inventariovacunacion.dominio.enumerator.TipoVacuna.JHONSON_AND_JHONSON")
     Iterable<Empleado> obtenerEmpleadosJhonsonAndJhonson();
 
     @Query("SELECT e FROM Empleado e JOIN e.vacuna v WHERE v.fechaVacunacion BETWEEN ?1 AND ?2")
